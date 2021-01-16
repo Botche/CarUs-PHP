b7085f817c75ed856278877dd163003579400161
Gabriel <47752724+Botche@users.noreply.github.com>
2021-01-17 01:57:29 +0200

    Add template page on / route

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        // Get data from DB in here and pass it to the view
+
+        return view('index.index', [
+            'title' => 'Welcome to CarUs!',
+            'text' => '
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item One</a>
+                        </h4>
+                        <h5>$24.99</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                    </div>
+                </div>
+            </div>
+            '
+        ]);
+    }
+}
